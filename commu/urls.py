2e6d15f6f28d94a71aa72b47a54fdcf743cd083a
chyygga <chyygga@gmail.com>
2022-05-16 22:31:31 +0500

    deploy

--- commu/urls.py
+++ commu/urls.py
@@ -16,17 +16,17 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.template.defaulttags import url
+# from django.template.defaulttags import url
 from django.urls import path, include
-from django.views.static import serve
+# from django.views.static import serve
 
 from info.views import pageNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
